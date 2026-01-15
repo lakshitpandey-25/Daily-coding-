52c8f8bdeb47a8265ae6b6dbed24d8586f6c6d9d
Lakshit Pandey <pandeylakshit52@gmail.com>
2026-01-15 19:03:01 +0530

    adding my c file to repo

---
+++ main.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+/*int main()
+{
+    int a,i ;
+    float b;
+    char c 
+    printf("the table is:- ");
+    scanf("%d", &a);
+
+    for(i=1;i<11;i++)
+
+    printf("%d*%d=%d\n",a,i,a*i);
+
+    printf("tell the number what you want:- ");
+    scanf(%d,&c);
+
+    return 0;
+}
+
+
+int main() {
+    int a;
+    printf("Enter a number: ");
+    scanf("%d", &a);
+    if (a < 5) {
+        if (a == 2) {
+            printf("Good boy");
+        } else {
+            printf("Not good");
+        }
+    } else {
+        printf("Bad");
+    }
+    return 0;
+}
+int main()
+{
+    int i ;
+    printf("enter a number %i");
+    scanf("%i",&i);
+
+    if (i==0)
+    {
+        printf("same");
+        
+    }
+    else;
+    {
+    printf("nothing");
+    }
+
+    return 0;
+
+}*/
+
+
+
+
+
+
+
+#include <stdio.h>
+int main(){
+    int a=5,b=6;
+    printf("The sum of two numbers is: %d",a+b);
+    return 0;
+}
+
